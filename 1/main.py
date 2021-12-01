500fa30786b0c83fe0582da157b6234a0630a369
Nick Tolhurst <nick.tolhurst@drdoctor.co.uk>
2021-12-01 18:31:10 +0000

    init commit

---
+++ 1/main.py
@@ -0,0 +1,15 @@
+with open('input', encoding='utf-8-sig') as f:
+    lines = f.readlines()
+
+oldValue = 0
+increments = 0
+
+for i,_ in enumerate(lines):
+    sumOfWindow = 0 if i < 2 else float(lines[i]) + float(lines[i - 1]) + float(lines[i - 2])
+
+    if oldValue != 0 and sumOfWindow > oldValue:
+        increments = -(~increments)
+    
+    oldValue = sumOfWindow
+
+print(increments)
