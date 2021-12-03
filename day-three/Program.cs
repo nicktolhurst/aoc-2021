16d5612b236c07ea37f8750d16bd5339b6c29f68
Nick Tolhurst <nick.tolhurst@drdoctor.co.uk>
2021-12-03 14:40:10 +0000

    updates program.cs

--- day-three/Program.cs
+++ day-three/Program.cs
@@ -1,24 +1,5 @@
-﻿string[] report = File.ReadAllLines(@"C:\Local\Personal\Advent\day-three\input");
+﻿var result1 = DayThree.PartOne();
+Console.WriteLine($"Part 1: {result1}");
 
-List<char> gammaRateBinary = new();
-
-for (int x = 0; x < 12; x++)
-{   
-    // Gets most common occurance of 'char' in binary string.
-    gammaRateBinary.Add(report
-        .Select(dat => dat[x])
-        .GroupBy(val => val)
-        .OrderByDescending(group => group.Count())
-        .First()
-        .First());
-}
-
-// Convert char arrays to string and flip for epsilon value
-var gammaStr = new string(gammaRateBinary.ToArray());
-var epsilonStr = new string(gammaStr.Select(c => c == '1' ? '0' : '1').ToArray());
-
-// Convert binary string to int32
-var gamma = Convert.ToInt32(gammaStr, 2);
-var epsilon = Convert.ToInt32(epsilonStr, 2);
-
-Console.WriteLine($"Part 1: {gamma*epsilon}");
+var result2 = DayThree.PartTwo();
+Console.WriteLine($"Part 2: {result2}");
